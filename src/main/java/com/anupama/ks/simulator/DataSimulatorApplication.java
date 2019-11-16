db4187a46018877c2e949d0b2c28213fbf9c76d8
Anupama <akhangar@cisco.com>
2019-11-16 16:38:23 +0530

    API produce data point in every two minute and send it to data consumer

--- src/main/java/com/anupama/ks/simulator/DataSimulatorApplication.java
+++ src/main/java/com/anupama/ks/simulator/DataSimulatorApplication.java
@@ -2,12 +2,31 @@ package com.anupama.ks.simulator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class DataSimulatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataSimulatorApplication.class, args);
+		scheduledataPoint();
+	}
+	
+	@Scheduled(cron = "0 0/02 * * * ?")
+	public static  void scheduledataPoint() {
+		
+		String data = "(1, 1)";
+		System.out.println(data);
+	    final String uri = "http://localhost:8082/user/consume?data="+data;
+	    System.out.println(uri);
+
+	    RestTemplate restTemplate = new RestTemplate();
+	    String result = restTemplate.getForObject(uri, String.class);
+
+	    System.out.println(result);
 	}
 
 }
